c91f3b3361fb5d6c55a64cf158ba9256d0cda489
AlanMatute7 <alan.matute@unah.hn>
2023-04-23 23:40:12 -0600

    Ejerc2
    Nuevo link tarea 2

---
+++ Ejerc2/Ejerc2/notas.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Ejerc2
+{
+    partial class notas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NotaFinalTextBox = new System.Windows.Forms.TextBox();
+            this.Nota4TextBox = new System.Windows.Forms.TextBox();
+            this.Nota3TextBox = new System.Windows.Forms.TextBox();
+            this.Nota2TextBox = new System.Windows.Forms.TextBox();
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NotaFinalTextBox
+            // 
+            this.NotaFinalTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NotaFinalTextBox.Location = new System.Drawing.Point(262, 301);
+            this.NotaFinalTextBox.Name = "NotaFinalTextBox";
+            this.NotaFinalTextBox.Size = new System.Drawing.Size(45, 26);
+            this.NotaFinalTextBox.TabIndex = 20;
+            this.NotaFinalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota4TextBox.Location = new System.Drawing.Point(262, 210);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota4TextBox.TabIndex = 19;
+            this.Nota4TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota3TextBox.Location = new System.Drawing.Point(262, 178);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota3TextBox.TabIndex = 18;
+            this.Nota3TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota2TextBox.Location = new System.Drawing.Point(262, 143);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota2TextBox.TabIndex = 17;
+            this.Nota2TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota1TextBox.Location = new System.Drawing.Point(262, 111);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota1TextBox.TabIndex = 16;
+            this.Nota1TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Notas Parciales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Nota 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Nota 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Nota 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Nota 4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(146, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Promedio";
+            // 
+            // notas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NotaFinalTextBox);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Name = "notas";
+            this.Text = "notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NotaFinalTextBox
+            // 
+            this.NotaFinalTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NotaFinalTextBox.Location = new System.Drawing.Point(262, 301);
+            this.NotaFinalTextBox.Name = "NotaFinalTextBox";
+            this.NotaFinalTextBox.Size = new System.Drawing.Size(45, 26);
+            this.NotaFinalTextBox.TabIndex = 20;
+            this.NotaFinalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota4TextBox.Location = new System.Drawing.Point(262, 210);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota4TextBox.TabIndex = 19;
+            this.Nota4TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota3TextBox.Location = new System.Drawing.Point(262, 178);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota3TextBox.TabIndex = 18;
+            this.Nota3TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota2TextBox.Location = new System.Drawing.Point(262, 143);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota2TextBox.TabIndex = 17;
+            this.Nota2TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota1TextBox.Location = new System.Drawing.Point(262, 111);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(45, 26);
+            this.Nota1TextBox.TabIndex = 16;
+            this.Nota1TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Notas Parciales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Nota 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Nota 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Nota 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Nota 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(146, 314);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Nota 1";
+            // 
+            // notas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NotaFinalTextBox);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Name = "notas";
+            this.Text = "notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Name = "notas";
+            this.Text = "notas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ejerc2/Ejerc2/notas.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejerc2
+{
+    public partial class notas : Form
+    {
+        public notas()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task<decimal> NotaFinalAsync(decimal num1, decimal num2, decimal num3, decimal num4)
+        {
+            decimal suma = await Task.Run(() =>
+            {
+                return (num1 + num2 + num3 + num4) / 4;
+
+            });
+            return suma;
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            decimal Resultado = await NotaFinalAsync(Convert.ToDecimal(Nota1TextBox.Text), Convert.ToDecimal(Nota2TextBox.Text),
+                                                     Convert.ToDecimal(Nota3TextBox.Text), Convert.ToDecimal(Nota4TextBox.Text));
+            NotaFinalTextBox.Text = Resultado.ToString();
+            if (Resultado >= 60)
+            {
+                MessageBox.Show("El Alumno Aprobo con: " + Resultado);
+            }
+            else
+            {
+                MessageBox.Show("El Alumno Reprobo con: " + Resultado);
+            }
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nota1TextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+  
